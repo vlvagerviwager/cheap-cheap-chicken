7f78858b670ec8a127184af4d8bdb294bc76413e
zky829 <zky829@users.noreply.github.com>
2018-11-10 16:32:39 +0000

    Disable no-console, fix ESLint errors

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ import Twit from 'twit';
 /*
  * TODO:
  *
+ * - Host on free Heroku + wake it up with New Relic addon
  * - Use cron-job.org to hit /BOT_ENDPOINT to wake up app and make the bot tweet
  * - Unit tests (Jest)
  */
 
+// Setup
 dotenv.config();
 const app = express();
 const config = {
@@ -17,43 +19,49 @@ const config = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  }
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  },
 };
 
+// Set up Twit
+let T;
 try {
-  const T = new Twit(config.twitter);
-  console.log(`Ready to tweet! Send a GET or POST request to {SOMEWHERE}/${process.env.BOT_ENDPOINT}`);
-}
-catch (err) {
+  T = new Twit(config.twitter);
+  // eslint-disable-next-line no-console
+  console.log(`Ready to tweet! Send a GET or POST request to localhost/${process.env.BOT_ENDPOINT}`);
+} catch (err) {
+  // eslint-disable-next-line no-console
   console.error(err, 'Sorry, the .env file does not have the correct settings in order to tweet.');
 }
 
 app.use(express.static('public'));
 
-const postTweet = (statusText) => {
-  T.post('statuses/update', { status: statusText }, (err) => {
+const postTweet = (tweetText, res) => {
+  T.post('statuses/update', { status: tweetText }, (err) => {
     if (err) {
+      // eslint-disable-next-line no-console
       console.log('Error:', err);
       res.sendStatus(500);
-    }
-    else {
+    } else {
       res.sendStatus(200);
     }
   });
-}
+};
 
 function getRandomQuote() {
-  var data = fs.readFileSync(`${__dirname}/../src/data/quotes.json`, 'utf8');
-  const quotes = JSON.parse(data).quotes;
+  const data = fs.readFileSync(`${__dirname}/../src/data/quotes.json`, 'utf8');
+  const { quotes } = JSON.parse(data);
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   return randomQuote;
 }
 
-app.all('/' + process.env.BOT_ENDPOINT, (req, res) => {
-  postTweet(`We're makin' us a cake that you never seen before. #PaRappatheRapper #PaRappa #パラッパラッパー`);
+app.all(`/${process.env.BOT_ENDPOINT}`, (req, res) => {
+  const tweetText = `${getRandomQuote()} #PaRappatheRapper #PaRappa #パラッパラッパー`;
+  postTweet(tweetText, res);
 });
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log('@CheapCheapChkn bot is running on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, () => {
+  const { port } = listener.address();
+  // eslint-disable-next-line no-console
+  console.log(`@CheapCheapChkn bot is running on port ${port}`);
 });
